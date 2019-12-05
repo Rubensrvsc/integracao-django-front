c27660d85c2291b4a3a180d70b3e69fa386dc4d5
rubensrvsc <rubensspfc100@gmail.com>
2019-12-04 21:55:01 -0300

    add:aplicativo react

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,8 +58,7 @@ APPEND_SLASH=False
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:4200',
-    'http://localhost:8081/',
+    'https://localhost:4200',
 )
 
 ROOT_URLCONF = 'backend.urls'
